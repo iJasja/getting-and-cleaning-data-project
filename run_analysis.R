fa0288da980c14c204e078b55df09d49acd49512
Jasja de Vries <jasjadv@gmail.com>
2015-01-20 19:33:56 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -40,4 +40,36 @@ measurements <- rbind(train,test)
 
 columns  <- colnames(measurements)
 selected_columns <- (grepl("activity..",columns) | grepl("subject..",columns) | grepl("-mean..",columns) & !grepl("-meanFreq..",columns) & !grepl("mean..-",columns) | grepl("-std..",columns) & !grepl("-std()..-",columns))
-measurements <- measurements[selected_columns==TRUE];
+measurements <- measurements[selected_columns==TRUE]
+
+##############################################
+# Part 3: Name the activities in the dataset #
+##############################################
+measurements = merge(measurements,activity_labels,by='activityId',all.x=TRUE)
+columns  <- colnames(measurements)
+
+##############################################################
+# Part 4: label the data set with descriptive activity names # 
+##############################################################
+for (i in 1:length(columns)) 
+{
+        columns[i] = gsub("\\()","",columns[i])
+        columns[i] = gsub("-std$","standard_deviation",columns[i])
+        columns[i] = gsub("-mean","mean",columns[i])
+        columns[i] = gsub("^(t)","time",columns[i])
+        columns[i] = gsub("^(f)","frequency",columns[i])
+        columns[i] = gsub("([Gg]ravity)","gravity",columns[i])
+        columns[i] = gsub("([Bb]ody[Bb]ody|[Bb]ody)","body",columns[i])
+        columns[i] = gsub("[Gg]yro","gyro",columns[i])
+        columns[i] = gsub("AccMag","accelerate_magnitude",columns[i])
+        columns[i] = gsub("([Bb]odyaccjerkmag)","body_acc_jer_magnitude",columns[i])
+        columns[i] = gsub("JerkMag","jerk_magnitude",columns[i])
+        columns[i] = gsub("GyroMag","gyro_magnitude",columns[i])
+}
+colnames(measurements) <- columns
+
+##################################################################
+# Part 5: datast with average of each variable for each activity # 
+##################################################################
+measurements_no_id <- measurements[,names(measurements) != 'activityType'];
+
